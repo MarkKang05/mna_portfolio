c546e30669b45d777bdfd7258489f7c7401cd3c5
macbook13 <markkang05@macbook13ui-MacBookAir.local>
2021-10-03 18:40:34 +0900

    IOT_House: socket.io & Parsing Json

--- IOT_House/web/app.js
+++ IOT_House/web/app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.io = require('socket.io')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,13 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -36,6 +38,34 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express22' });
+  res.render('index', {
+    title1: 'title',
+    value1: 'value'
+  });
+});
+
+
+
+const json = `{
+  "led1" : 121,
+  "led2" : 222,
+  "led3" : 333
+}`;
+
+
+
+app.io.on('connection', function(socket){
+  console.log('connected..');
+  socket.on('disconnect', function () {
+    console.log('disconnected..');
+  })
+  setInterval(() => {
+    socket.emit('msg', json);
+  }, 3000);
 });
 
 module.exports = app;
